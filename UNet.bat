535364ccc2dc09c10b41bf05eab4840181e9af31
Khang Duong <sugarkhang02@gmail.com>
2025-11-26 04:31:43 -0600

    Update arguments in bash script

--- UNet.bat
+++ UNet.bat
@@ -3,16 +3,16 @@ setlocal enabledelayedexpansion
 
 for /l %%i in (0, 1, 10) do (
     set "a=R2U_Net"
-    python main.py --model_type=!a! --dataset="SkinCancer" --train_path="./data/"
-    python main.py --model_type=!a! --dataset="Lung" --train_path="./data/"
-    python main.py --model_type=!a! --dataset="CHASE_DB1" --train_path="./data/"
-    python main.py --model_type=!a! --dataset="STARE" --train_path="./data/"
+    python main.py --model_type=!a! --dataset="SkinCancer" --data_path="./data/" --augmentation_prob=0.4
+    python main.py --model_type=!a! --dataset="Lung" --data_path="./data/" --augmentation_prob=0.9
+    python main.py --model_type=!a! --dataset="CHASE_DB1" --data_path="./data/" --augmentation_prob=0.6
+    python main.py --model_type=!a! --dataset="STARE" --data_path="./data/" --augmentation_prob=0.6
 
     set "a=R2U_Net++"
-    python main.py --model_type=!a! --dataset="SkinCancer" --train_path="./data/"
-    python main.py --model_type=!a! --dataset="Lung" --train_path="./data/"
-    python main.py --model_type=!a! --dataset="CHASE_DB1" --train_path="./data/"
-    python main.py --model_type=!a! --dataset="STARE" --train_path="./data/"
+    python main.py --model_type=!a! --dataset="SkinCancer" --data_path="./data/" --augmentation_prob=0.4
+    python main.py --model_type=!a! --dataset="Lung" --data_path="./data/" --augmentation_prob=0.9
+    python main.py --model_type=!a! --dataset="CHASE_DB1" --data_path="./data/" --augmentation_prob=0.6
+    python main.py --model_type=!a! --dataset="STARE" --data_path="./data/" --augmentation_prob=0.6
 )
 
 endlocal
