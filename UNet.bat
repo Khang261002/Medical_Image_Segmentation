667cd2dbbb0a1ad1b952a1d19d844b555fc29324
Khang Duong <sugarkhang02@gmail.com>
2025-11-27 16:04:40 -0600

    Update value in UNet.bat script

--- UNet.bat
+++ UNet.bat
@@ -5,14 +5,14 @@ for /l %%i in (0, 1, 10) do (
     set "a=R2U_Net"
     python main.py --model_type=!a! --dataset="SkinCancer" --data_path="./data/" --augmentation_prob=0.4
     python main.py --model_type=!a! --dataset="Lung" --data_path="./data/" --augmentation_prob=0.9
-    python main.py --model_type=!a! --dataset="CHASE_DB1" --data_path="./data/" --augmentation_prob=0.6
-    python main.py --model_type=!a! --dataset="STARE" --data_path="./data/" --augmentation_prob=0.6
+    python main.py --model_type=!a! --dataset="CHASE_DB1" --data_path="./data/" --augmentation_prob=1.0
+    python main.py --model_type=!a! --dataset="STARE" --data_path="./data/" --augmentation_prob=1.0
 
     set "a=R2U_Net++"
     python main.py --model_type=!a! --dataset="SkinCancer" --data_path="./data/" --augmentation_prob=0.4
     python main.py --model_type=!a! --dataset="Lung" --data_path="./data/" --augmentation_prob=0.9
-    python main.py --model_type=!a! --dataset="CHASE_DB1" --data_path="./data/" --augmentation_prob=0.6
-    python main.py --model_type=!a! --dataset="STARE" --data_path="./data/" --augmentation_prob=0.6
+    python main.py --model_type=!a! --dataset="CHASE_DB1" --data_path="./data/" --augmentation_prob=1.0
+    python main.py --model_type=!a! --dataset="STARE" --data_path="./data/" --augmentation_prob=1.0
 )
 
 endlocal
